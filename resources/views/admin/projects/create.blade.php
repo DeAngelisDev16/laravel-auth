c8d434004fa10d1646b0a903410eb8345f34d088
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-21 16:49:29 +0100

    create form setup

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -5,6 +5,23 @@
     <h3>
         Fill the following form to add a new project:
     </h3>
+    <form action="{{route('admin.projects.store')}}">
+        <div class="mb-3">
+          
+          <input type="text" class="form-control" placeholder="Add project title" name="title" value="{{old('title')}}" >
+          
+        </div>
+        <div class="mb-3">
+          
+          <input type="text" class="form-control" placeholder="Add project reference" name="github_reference" value="{{old('github_reference')}}" >
+        </div>
+        <div class="mb-3">
+          
+            <input type="text" class="form-control" placeholder="Add project description" name="description" value="{{old('description')}}" >
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
 
 </div>
 @endsection
