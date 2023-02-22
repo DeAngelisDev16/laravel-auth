073fee5247af080091f4bbe46a73b5e704127bd4
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-22 10:07:48 +0100

    unique form create/edit setup

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -5,41 +5,7 @@
     <h3>
         Fill the following form to add a new project:
     </h3>
-    <form action="{{route('admin.projects.store')}}" method="POST">
-        @csrf
-
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                    
-                @foreach ($errors->all() as $error)
-                <li>
-                    {{$error}}
-                </li>
-                
-                @endforeach
-                
-            </ul>
-            
-        </div>
-        @endif
-                
-        <div class="mb-3">
-          
-          <input type="text" class="form-control" placeholder="Add project title" name="title" value="{{old('title')}}" >
-          
-        </div>
-        <div class="mb-3">
-          
-          <input type="text" class="form-control" placeholder="Add project reference" name="github_reference" value="{{old('github_reference')}}" >
-        </div>
-        <div class="mb-3">
-          
-            <input type="text" class="form-control" placeholder="Add project description" name="description" value="{{old('description')}}" >
-        </div>
-        
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+    @include('admin.projects.form', ['method'=>'POST','routeAddress'=>'admin.projects.store'])
 
 </div>
 @endsection
---
+++ resources/views/admin/projects/form.blade.php
@@ -0,0 +1,39 @@
+
+    <form action="{{route($routeAddress)}}" method="POST">
+        @csrf
+        @method($method)
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                    
+                @foreach ($errors->all() as $error)
+                <li>
+                    {{$error}}
+                </li>
+                
+                @endforeach
+                
+            </ul>
+            
+        </div>
+        @endif
+                
+        <div class="mb-3">
+          
+          <input type="text" class="form-control" placeholder="Add project title" name="title" value="{{old('title', $project->title)}}" >
+          
+        </div>
+        <div class="mb-3">
+          
+          <input type="text" class="form-control" placeholder="Add project reference" name="github_reference" value="{{old('github_reference', $project->github_reference)}}" >
+        </div>
+        <div class="mb-3">
+          
+            <input type="text" class="form-control" placeholder="Add project description" name="description" value="{{old('description', $project->description)}}" >
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+</div>
